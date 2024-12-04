b3982729f8856aa01475e0b52db8beb809cda3a8
Juan Diego Guzmán Guzmán <guzman.jdiego@outlook.com>
2024-12-04 17:44:51 -0600

    Delete Sistema Bancario directory

--- Sistema Bancario/src/usuario/empleado/GerenteSucursal.java
+++
@@ -1,9 +0,0 @@
-package usuario.empleado;
-
-public class GerenteSucursal extends Empleado{
-
-    public GerenteSucursal(String IdEmpleado, String nombre, String apellidos, String RFC, String CURP, String direccion, String contrasenia, String sucursal, double salario) {
-        super(IdEmpleado, nombre, apellidos, RFC, CURP, direccion, contrasenia, sucursal, salario, RolTrabajo.GERENTESUCURSAL);
-    }
-
-}
--- Sistema Bancario/src/usuario/empleado/RolTrabajo.java
+++
@@ -1,7 +0,0 @@
-package usuario.empleado;
-
-public enum RolTrabajo {
-    // Roles del trabajador
-    GERENTESUCURSAL,
-    EJECUTIVOCUENTA
-}
